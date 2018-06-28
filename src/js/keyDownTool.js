cfe08cf791a97910f0607eff33282aa9d426f159
xiaoyao <xiaoyao9524@163.com>
2018-06-28 17:09:19 +0800

    优化了部分逻辑，完成了down的逻辑

--- src/js/keyDownTool.js
+++ src/js/keyDownTool.js
@@ -1,6 +1,81 @@
 import $ from 'jquery'
 import {data} from './data'
 import {renderGame} from './tool'
+
+export function moveUpOrDown (direction, obj) {
+  // obj: 当前检测的格子信息
+  // x: 当前检测的格子x
+  // y: 当前检测格子的y
+  // val: 当前检测格子的数值
+  let {x, y, val} = obj;
+  console.log('当前检测的格子信息: ', obj);
+  checkNext();
+  // console.log('flag: ', flag);
+  function checkNext () {
+    let flag = null;
+    if (direction === 'up') {
+      flag = y <= 0 ? true : false;
+    } else if (direction === 'down') {
+      flag = y >= 3 ? true : false;
+    }
+    if (flag) {
+      console.log('y到头了, row:', y);
+      return check({x, y})
+    } else {
+      if (direction === 'up') {
+        y--;
+      } else if (direction === 'down') {
+        y++;
+      }
+      console.log('检查第', y, '行');
+      let flag = check({x, y});
+      console.log('检查结果: ', flag);
+      if (!flag) {
+        // 说明目标位置为空
+        console.log('目标位置为空, row: ', y)
+        let flag2 = null;
+        if (direction === 'up') {
+          flag2 = y > 0 ? true : false;
+        } else if (direction === 'down') {
+          flag2 = y < 3 ? true : false;
+        }
+        if (flag2) {
+          console.log('第', y, `行为空，继续往${direction}查找`);
+          return checkNext();
+        } else {
+          // console.log('已经是最后一行了（最上方）,开始移动');
+          let aims = {
+            x,
+            y,
+            val: null
+          };
+          moveTo(obj, aims);
+          return null;
+        }
+      } else {
+        // 说明目标位置不为空
+        console.log('目标位置不为空, row: ', y, 'column: ', x);
+        let to = check ({x, y});
+        console.log('目标位置为：', to, val);
+
+        if (to.value === val) {
+          console.log('val一样')
+          moveTo(obj, to);
+        } else {
+          console.log('val不一样')
+          if (direction === 'up') {
+            moveTo(obj, {x, y: y + 1});
+          } else if (direction === 'down') {
+            moveTo(obj, {x, y: y - 1});
+          }
+        }
+        console.log(data);
+        return flag;
+      }
+    }
+  }
+}
+
 // 检查某个位置
 export function check (obj) {
   let {x, y} = obj;
